63456de2f74489e22a71b4273296fd23304a51c6
Enrico Ludwig <enrico.ludwig@zion-networks.de>
2024-08-19 13:08:44 +0200

    Added loading Twitch secret from file

--- App.axaml.cs
+++ App.axaml.cs
@@ -1,6 +1,7 @@
+using System;
+using System.IO;
 using Avalonia;
 using Avalonia.Controls.ApplicationLifetimes;
-using Avalonia.Data.Core;
 using Avalonia.Data.Core.Plugins;
 using Avalonia.Markup.Xaml;
 using BetterRaid.ViewModels;
@@ -10,8 +11,26 @@ namespace BetterRaid;
 
 public partial class App : Application
 {
+    public static string TokenClientId = "";
+    public static string TokenClientSecret = "";
+
     public override void Initialize()
     {
+        try
+        {
+            var tokenFile      = "zn_twitch.secret";
+            var profilePath    = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile);
+            var tokenFilePath  = Path.Combine(profilePath, tokenFile);
+            var tokenFileLines = File.ReadAllLines(tokenFilePath);
+            TokenClientId      = tokenFileLines[0].Split('=')[1];
+            TokenClientSecret  = tokenFileLines[1].Split('=')[1];
+        }
+        catch (Exception)
+        {
+            Console.WriteLine("[ERROR] Failed to read token from secret file!");
+            Environment.Exit(1);
+        }
+
         AvaloniaXamlLoader.Load(this);
     }
 
