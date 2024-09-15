e9ce00d7b66a34c26eab97bc2c53db855f2ca972
Enrico Ludwig <enrico.ludwig@zion-networks.de>
2024-09-15 18:26:00 +0200

    Resolved #14 - channel name text box is now focussed

--- Controls/ChannelsPage.axaml.cs
+++ Controls/ChannelsPage.axaml.cs
@@ -1,3 +1,5 @@
+using Avalonia;
+using Avalonia.Controls;
 using Avalonia.Markup.Xaml;
 using Avalonia.ReactiveUI;
 using BetterRaid.ViewModels;
@@ -11,5 +13,20 @@ public partial class ChannelsPage : ReactiveUserControl<ChannelsPageViewModel>
     {
         this.WhenActivated(_ => { });
         AvaloniaXamlLoader.Load(this);
+    }
+
+    private void OnAddChannelPanelPropertyChanged(object? sender, AvaloniaPropertyChangedEventArgs e)
+    {
+        if (sender is not Border border)
+            return;
+
+        if (e.Property != IsVisibleProperty)
+            return;
+
+        if (border.IsVisible is false)
+            return;
+
+        // direct access results in NullReferenceException - don't ask me why
+        border.FindControl<TextBox>("ChannelNameTextBox")?.Focus();
     }
 }
